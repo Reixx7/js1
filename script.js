9d60a0c3e164749601d1feb7e05f182ad0d0cb4b
Reixx7 <reixx23and7@gmail.com>
2025-03-11 10:08:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let name = prompt("Ismingizni kiriting");
+let vozrast = prompt("Yoshingizni kiriting");
+let adres = prompt("Manzilingizni kiriting");
+
+alert(`Salom mening ismim ${name} va men ${vozrast} yosdaman. Men ${adres} danman`)
